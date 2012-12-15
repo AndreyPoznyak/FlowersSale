e49244ff506575c5cd4983c9e5df5cdb20d06044
AndreyPoznyak <andpoznyak@gmail.com>
2012-12-16 00:48:50 +0300

    require.js added to the project

---
+++ FlowersSale/require-config.js
@@ -0,0 +1,25 @@
+﻿require.config({
+
+		waitSeconds: 60,
+
+		baseUrl: "js",
+
+		deps: ["base"],
+		
+		paths: {
+				// Libraries
+				jquery: "jquery-1.8.0",
+				lodash: "lodash",
+				backbone: "backbone"
+		},
+
+		shim: {
+				backbone: {
+						deps: ["lodash", "jquery"],
+						exports: "Backbone"
+				},
+				"jquery.easing.1.3": ["jquery"],
+				"base": ["jquery", "jquery.easing.1.3"]
+		}
+});
+
