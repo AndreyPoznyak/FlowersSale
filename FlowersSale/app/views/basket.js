444f3c3b02b60c3ad214fbec7cc683b4ee4c6f76
Andrey Poznyak <andpoznyak@gmail.com>
2013-02-06 15:28:11 +0300

    scroll bar

--- FlowersSale/app/views/basket.js
+++ FlowersSale/app/views/basket.js
@@ -1,12 +1,12 @@
-﻿define([
+﻿define( [
 		"backbone",
 		"app/views/basket-item",
 		"js/jquery.tinyscrollbar.min"
 ],
-	function (Backbone, BasketItemView) {
-			var BasketView = Backbone.View.extend({
+	function ( Backbone, BasketItemView ) {
+			var BasketView = Backbone.View.extend( {
 
-					template: $("#basket-template"),
+					template: $( "#basket-template" ),
 
 					basketItemsById: {},
 
@@ -16,47 +16,47 @@
 
 					initialize: function () {
 							var view = this;
-							view.listenTo(view.model, "change:opened", function (event, opened) {
-									if (opened === true) {
+							view.listenTo( view.model, "change:opened", function ( event, opened ) {
+									if ( opened === true ) {
 											view.$el.fadeIn();
-											$(".dark-mask").fadeIn();
-											view.$el.find(".basket-long-content").tinyscrollbar();
-											view.$el.find(".basket-long-content").tinyscrollbar_update("relative");
+											$( ".dark-mask" ).fadeIn();
+											//view.$el.find(".basket-long-content").tinyscrollbar_update("relative");
 									} else {
 											view.$el.fadeOut();
-											$(".dark-mask").fadeOut();
+											$( ".dark-mask" ).fadeOut();
 									}
-							});
+							} );
 
-							view.listenTo(view.model.get("ordersCollection"), "add", function (order) {
-									var $itemNode = $("<tr></tr>", {
+							view.listenTo( view.model.get( "ordersCollection" ), "add", function ( order ) {
+									var $itemNode = $( "<tr></tr>", {
 											id: "item" + order.id
-									});
-									view.$el.find(".basket-items-table-body").append($itemNode);
-									var itemView = new BasketItemView({
+									} );
+									view.$el.find( ".basket-items-table-body" ).append( $itemNode );
+									var itemView = new BasketItemView( {
 											model: order,
 											el: $itemNode
-									});
+									} );
 									view.basketItemsById[order.id] = itemView;
-							});
+							} );
 
-							view.listenTo(view.model.get("ordersCollection"), "remove", function (order) {
-									view.$el.find(".basket-items-table-body").children("#" + "item" + order.id).remove();
+							view.listenTo( view.model.get( "ordersCollection" ), "remove", function ( order ) {
+									view.$el.find( ".basket-items-table-body" ).children( "#" + "item" + order.id ).remove();
 									delete view.basketItemsById[order.id];
-									view.$el.find(".basket-long-content").tinyscrollbar_update("relative");
-							});
+									//view.$el.find(".basket-long-content").tinyscrollbar_update("relative");
+							} );
 							view.render();
 					},
 
 					render: function () {
 							var view = this;
-							view.$el.html(view.template.html());
+							view.$el.html( view.template.html() );
 					},
 
 					close: function () {
-							this.model.set("opened", false);
+							this.$el.find( ".basket-long-content" ).tinyscrollbar();
+							//this.model.set( "opened", false );
 					}
-			});
+			} );
 
 			return BasketView;
-	});
+	} );
